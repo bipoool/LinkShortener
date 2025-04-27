a38f205701fef0508768a417d6337e5eb364ad9e
bipoool <mail@vipulgupta.me>
2025-04-27 13:56:45 +0000

    Start row added in SQL

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -5,3 +5,8 @@ CREATE TABLE links (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
     expires_at TIMESTAMP WITH TIME ZONE
 );
+
+INSERT INTO
+    links (id, short_code, original_url)
+VALUES
+    (100000, '000Q0u', 'https://example.com');
